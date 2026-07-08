c34a09dfb6fae32b6bf197a096e07e8cba9bdf68
Hamlet Eranosyan <Ham.Eran@yandex.ru>
2026-07-08 15:25:18 +0300

    feat: update ArticleModel, create CategoryArticleModel

--- src/blog_app/infrastructure/database/models/articles.py
+++ src/blog_app/infrastructure/database/models/articles.py
@@ -1,3 +1,4 @@
+from datetime import UTC, datetime
 from uuid import UUID, uuid4
 
 from sqlalchemy.orm import Mapped, mapped_column
@@ -12,3 +13,13 @@ class ArticleModel(Base):
     is_active: Mapped[bool] = mapped_column(default=True)
     title: Mapped[str] = mapped_column(String(100))
     content: Mapped[str] = mapped_column(Text)
+    category: Mapped[int | None] = mapped_column(foreign_key="categories.id")
+    image_url: Mapped[str | None]
+    created_at: Mapped[datetime] = mapped_column(default=datetime.now(UTC))
+    updated_at: Mapped[datetime] = mapped_column(default=datetime.now(UTC))
+
+
+class CategoryArticleModel(Base):
+    __tablename__ = "categories"
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    name: Mapped[str] = mapped_column(String(100))
