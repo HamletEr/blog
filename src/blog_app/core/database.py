0046b1cfa5ca3bc44cbf9232ca786a74b96314c1
Hamlet Eranosyan <Ham.Eran@yandex.ru>
2026-07-08 19:14:54 +0300

    added dummy config for db, create(start) real db repository

--- src/blog_app/core/database.py
+++ src/blog_app/core/database.py
@@ -1,5 +1,17 @@
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 from sqlalchemy.orm import DeclarativeBase
 
 
 class Base(DeclarativeBase):
     pass
+
+
+async_engine = create_async_engine("")  # TODO
+
+AsyncSessionLocal = async_sessionmaker(  # TODO
+    bind=async_engine,
+    class_=AsyncSession,
+    expire_on_commit=False,
+    autocommit=False,
+    autoflush=False,
+)
---
+++ src/blog_app/infrastructure/repositories/articles.py
@@ -0,0 +1,48 @@
+from uuid import UUID
+
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from blog_app.domain.entities.articles import Article, ArticleData
+from blog_app.domain.repositories.articles import ArticleRepository
+from blog_app.infrastructure.models.articles import ArticleModel
+
+
+class PGArticleRepository(ArticleRepository):
+    def __init__(self, session: AsyncSession) -> None:
+        self._session = session
+
+    @staticmethod
+    def _model_to_domain(model: ArticleModel) -> Article:
+        return Article(
+            id=model.id,
+            is_active=model.is_active,
+            data=ArticleData(
+                title=model.title,
+                content=model.content,
+                category=model.category,
+                image_url=model.image_url,
+            ),
+            created_at=model.created_at,
+            updated_at=model.updated_at,
+        )
+
+    @staticmethod
+    def _domain_to_model(domain: Article) -> ArticleModel:
+        return ArticleModel(
+            id=domain.id,
+            is_active=domain.is_active,
+            title=domain.data.title,
+            content=domain.data.content,
+            category=domain.data.category,
+            image_url=domain.data.image_url,
+            created_at=domain.created_at,
+            updated_at=domain.updated_at,
+        )
+
+    async def get_by_id(self, article_id: UUID) -> Article | None:
+        stmt = select(ArticleModel).where(ArticleModel.id == article_id)
+        result = await self._session.execute(stmt)
+        article: ArticleModel | None = result.scalar_one_or_none()
+
+        return self._model_to_domain(article) if article else None
