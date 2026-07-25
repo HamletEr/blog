fd0d9b5cfb63aa4f1269f8c564fb618e6a6c87a1
Hamlet Eranosyan <Ham.Eran@yandex.ru>
2026-07-25 14:25:24 +0300

    feat: redis added

--- src/blog_app/core/config.py
+++ src/blog_app/core/config.py
@@ -35,6 +35,10 @@ def get_db_dsn(async_: bool = True) -> str:
     return str(db_dsn)
 
 
+def get_redis_dsn() -> str:
+    return f"redis://{_settings.redis.host}:{_settings.redis.port}/{_settings.redis.db}"
+
+
 class Settings(BaseSettings):
     app_name: str = _settings.app_name
     app_version: str = _settings.version
@@ -43,6 +47,7 @@ class Settings(BaseSettings):
     app_env: str = _settings.app_env
     db_dsn_async: str = get_db_dsn(async_=True)
     db_dsn_sync: str = get_db_dsn(async_=False)
+    redis_dsn: str = get_redis_dsn()
     debug: bool = _settings.debug
     jwt_algorithm: str = _settings.jwt.algorithm
     jwt_access_ttl_minutes: int = int(_settings.jwt.access_token_expire_minutes)
